856c755579ea6e3aa0a3163dcdf676bc6f892c2a
plbpeter <35516816+plbpeter@users.noreply.github.com>
2025-09-18 14:13:29 +1000

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+terraform {
+  cloud {
+
+    organization = "PetersTest"
+
+    workspaces {
+      name = "COM_Workspace"
+    }
+  }
+}
